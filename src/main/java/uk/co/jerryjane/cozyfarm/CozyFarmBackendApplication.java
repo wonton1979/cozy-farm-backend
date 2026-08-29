19aa9969a07fdb2e1017e781a0aa426ece7fa243
Yejun Guan <guanyejun79@gmail.com>
2026-08-29 20:46:50 +0100

    chore: initialize Cozy Farm Spring Boot backend

---
+++ src/main/java/uk/co/jerryjane/cozyfarm/CozyFarmBackendApplication.java
@@ -0,0 +1,13 @@
+package uk.co.jerryjane.cozyfarm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CozyFarmBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CozyFarmBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uk/co/jerryjane/cozyfarm/CozyFarmBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.co.jerryjane.cozyfarm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CozyFarmBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
